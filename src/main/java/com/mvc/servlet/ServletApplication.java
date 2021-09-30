0290410e92a6528737599e588b66e77cbb9c6768
woody <woojihye2339@gmail.com>
2021-09-30 19:07:46 +0900

    feature : @ServletComponentScan 추가
    > 현재 클래스의 패키지 하위에 속한 모든 서블릿을 자동 등록

--- src/main/java/com/mvc/servlet/ServletApplication.java
+++ src/main/java/com/mvc/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package com.mvc.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // mvc.servlet 하위 패키지들을 스캔하여 모든 서블릿을 자동 등록
 @SpringBootApplication
 public class ServletApplication {
 
